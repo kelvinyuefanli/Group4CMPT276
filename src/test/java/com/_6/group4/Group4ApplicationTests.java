5b4821b8e08f1ee4e56baff46f2a157dfd158888
Kaden Nguyen <kaden@Kadens-MacBook-Pro-39.local>
2026-02-19 18:41:16 -0800

    init push

---
+++ src/test/java/com/_6/group4/Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
